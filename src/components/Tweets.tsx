b9bc00f44c7c6bd0e60f8634b41eeb66151c7910
Alejo <alejoivanrojas@gmail.com>
2023-01-16 14:32:41 -0300

    thunk completed

--- src/components/Tweets.tsx
+++ src/components/Tweets.tsx
@@ -1,18 +1,25 @@
-import { useAppSelector } from '../redux/hooks'
+import { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../redux/hooks'
+import { getPosts } from '../redux/tweetSlice'
 import Tweet from './Tweet'
+
+
 
 
 const Tweets = () => {
     const data = useAppSelector(state => state.data)
+    const dispatch = useAppDispatch()
 
     return (
     <>
         {data.length 
-            ? data.map(tweet => (
+            ? (data.map(tweet => (
                 <Tweet content={tweet} />
               ))
+              )
             : null
         }
+        <button onClick={()=> dispatch(getPosts())}>Fetch More</button>
     </>
   )
 }
--- src/redux/tweetSlice.tsx
+++ src/redux/tweetSlice.tsx
@@ -1,18 +1,38 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface IState{
     data: string[]
 }
 
+interface Posts {
+    body: string
+  }
+
 const initialState: IState = {data: []}
+
+export const getPosts = createAsyncThunk("tweets/getPosts", async()=>{
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data: Posts[] = await res.json()
+  
+    const posts = data.map(post => post.body)
+  
+    return posts
+})
+
 
 const tweetSlice = createSlice({
     name: "tweets",
     initialState,
     reducers: {
         addTweet: (state, action: PayloadAction<string>)=>{
-            state.data.push(action.payload)
+            state.data.unshift(action.payload)
         }
+    },
+    extraReducers: builder =>{
+        builder
+            .addCase(getPosts.fulfilled, (state, action: PayloadAction<string[]>)=>{
+                state.data.push(...action.payload)
+            })
     }
 })
 
